c767bfce9d78328059ee6a5c61cc262b2ba9b149
169357683@qq.com <dxp971216>
2019-12-12 17:13:42 +0800

    commit

--- src/main/java/jianzhioffer/LinkedClone.java
+++ src/main/java/jianzhioffer/LinkedClone.java
@@ -1,15 +1,13 @@
 package jianzhioffer;
-
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @Description 复杂链表的复制
  * @auther DuanXiaoping
  * @create 2019-12-11 21:32
  */
+
+
 public class LinkedClone {
     class RandomListNode {
         int label;
@@ -44,22 +42,31 @@ public class LinkedClone {
         srcP = pHead;
         copy = srcP.next;
         RandomListNode head = copy;
-        while (copy != null) {
-            copy.next = copy.next.next;
-            copy = copy.next.next;
+        while (copy.next != null) {
+            srcP = copy.next;
+            copy.next = srcP.next;
+            copy = srcP.next;
         }
         return head;
     }
 
     public RandomListNode createLinked() {
+        List<RandomListNode> listNodes = new ArrayList<>();
         RandomListNode head = new RandomListNode(random.nextInt(1000));
+        listNodes.add(head);
         RandomListNode tempNode = head;
         int i = 0;
         while (i < 10) {
             RandomListNode newNode = new RandomListNode(random.nextInt(1000));
+            listNodes.add(newNode);
             tempNode.next = newNode;
             tempNode = newNode;
             i++;
+        }
+        tempNode = head;
+        while (tempNode != null) {
+            tempNode.random = listNodes.get(random.nextInt(11));
+            tempNode = tempNode.next;
         }
         return head;
     }
@@ -67,14 +74,20 @@ public class LinkedClone {
     public static void main(String[] args) {
         LinkedClone test = new LinkedClone();
         RandomListNode head = test.createLinked();
+        RandomListNode arg = head;
+        Integer lab = 0;
         while (head != null) {
-            if (head.next != null) {
-                System.out.println(head.label + "->" + head.next.label);
-            } else {
-                System.out.println(head.label + "->" + head.next);
-            }
-//            System.out.println(head.label);
+            System.out.print(head.label + "->" + (lab = head.next == null ? null : head.next.label) + " ");
+            System.out.print(head.label + "->" + (lab = head.random == null ? null : head.random.label) + "#");
             head = head.next;
+        }
+        RandomListNode head2 = test.Clone(arg);
+        System.out.println();
+        Integer lab2 = 0;
+        while (head2 != null) {
+            System.out.print(head2.label + "->" + (lab2 = head2.next == null ? null : head2.next.label) + " ");
+            System.out.print(head2.label + "->" + (lab2 = head2.random == null ? null : head2.random.label) + "#");
+            head2 = head2.next;
         }
     }
 }
